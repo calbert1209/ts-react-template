b947f7d1fbb40438a39e3a899a621dd7b41ad510
Christian Albert <calbert1209@gmail.com>
2022-06-01 21:12:00 +0900

    prep eslint for use with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,20 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier'
   ],
   env: {
     browser: true,
     es6: true,
     jest: true,
     node: true,
+  },
+  settings: {
+    react: {
+      version: "18.0.10"
+    }
   }
 };
