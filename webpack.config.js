312626da66db28c2cbcdceea63b80f115c6d2c54
Christian Albert <calbert1209@gmail.com>
2022-06-01 20:24:21 +0900

    remove unused second entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     index: './src/index.tsx',
-    print: './src/print.ts'
   },
   devtool: 'inline-source-map',
   module: {
